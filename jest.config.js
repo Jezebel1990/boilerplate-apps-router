6576abb3d0c619f5b906a6d65df90f7aab706609
Jezebel1990 <jezebelguedes@gmail.com>
2024-10-19 01:07:07 -0300

    Initial jest test

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,4 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts(x)?'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  }
 }
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Main = () => (
+    <main>
+        <h1>React Avançado</h1>
+    </main>
+)
+export default Main
