52c386910c7592fe12d9b336c3af5a5a581214a1
Jezebel1990 <jezebelguedes@gmail.com>
2024-10-21 12:38:57 -0300

    Add snapshots com jest styled components

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,22 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest' // Transforma arquivos TypeScript e TSX
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }], // Ajuste a configuração do ts-jest aqui
+  },
+  moduleNameMapper: {
+    '^styled-components': 'styled-components/dist/styled-components.browser.cjs.js', // Correção da sintaxe do mapper
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'], // Ignora diretórios do Next.js e node_modules nos testes
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.jest.json' // Caminho para o tsconfig específico do Jest
-    }
-  },
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Arquivo de setup para inicializações de testes
 
   collectCoverage: true, // Habilita a coleta de cobertura de testes
   collectCoverageFrom: [
     'src/**/*.ts(x)?', // Coleta cobertura de arquivos .ts e .tsx
     '!src/app/**', // Ignora a pasta app
-    '!src/lib/registry.tsx' // Ignora o arquivo registry.tsx
+    '!src/lib/registry.tsx', // Ignora o arquivo registry.tsx
+    '!src/types/**',
+    '!src/styles/**'
   ],
-  modulePaths: ['<rootDir>/src/'] // Permite importar módulos a partir de 'src' sem precisar de caminhos relativos
-}
+  modulePaths: ['<rootDir>/src/'], // Permite importar módulos a partir de 'src' sem precisar de caminhos relativos
+};
