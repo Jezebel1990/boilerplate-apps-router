073622a5b49153ccfc6402c18e17c5a888e9fc8b
Jezebel1990 <jezebelguedes@gmail.com>
2024-10-21 02:10:53 -0300

    Add Styled components

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,23 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest' // Transforma arquivos TypeScript e TSX
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'], // Ignora diretórios do Next.js e node_modules nos testes
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.jest.json' // Caminho para o tsconfig específico do Jest
+    }
+  },
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Arquivo de setup para inicializações de testes
+
+  collectCoverage: true, // Habilita a coleta de cobertura de testes
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?', // Coleta cobertura de arquivos .ts e .tsx
+    '!src/app/**', // Ignora a pasta app
+    '!src/lib/registry.tsx' // Ignora o arquivo registry.tsx
+  ],
+  modulePaths: ['<rootDir>/src/'] // Permite importar módulos a partir de 'src' sem precisar de caminhos relativos
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-const Main = () => (
-    <main>
-        <h1>React Avançado</h1>
-    </main>
+
+import * as S from './styles'
+
+const Main = ({
+      title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+    <S.Wrapper>
+        <S.Title>{title}</S.Title>
+        <S.Description>{description}</S.Description>
+    </S.Wrapper>
 )
 export default Main
